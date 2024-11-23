f76641852cb840ce0bd9cc120a78c9bb9aa4ffa9
Vega <anudvegtummala@gmail.com>
2024-11-23 16:53:02 +0530

    graphs list and matrix implementation

--- graphs/adjacentcy_list.py
+++ graphs/adjacentcy_list.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def add_edge(adj_list,i,j):
     adj_list[i].append(j)
     adj_list[j].append(i)
@@ -38,8 +40,43 @@ def print_list(adj_list):
     for row in adj_list:
         print(row)
 
-def search_vertex_dfs(vertex):
-    
+def search_vertex_bfs(adj_list, vertex_to_search):
+    queue_to_visit = deque()
+    queue_to_visit.append(0)
+    visited_nodes = [False for _ in range(len(adj_list))]
+    while queue_to_visit:
+        current_vertex = queue_to_visit.popleft()
+        visited_nodes[current_vertex] = True
+        print("current vertex: ",current_vertex)
+        if vertex_to_search == current_vertex:
+            print("Item found ",current_vertex)
+            return
+        if len(adj_list[current_vertex]) > 0:
+            for vertex in adj_list[current_vertex]:
+                if visited_nodes[vertex] is False and vertex not in queue_to_visit:
+                    queue_to_visit.append(vertex)
+    print("item not found")
+    return
+
+
+def search_vertex_dfs(adj_list, vertex_to_search):
+    n = len(adj_list)
+    visited_nodes = [False for _ in range(0,n)]
+    to_visited_stack = []
+    to_visited_stack.append(0)
+    while to_visited_stack:
+        currrent_vertex = to_visited_stack.pop()
+        visited_nodes[currrent_vertex] = True
+        print("Current vertex: ",currrent_vertex)
+        if vertex_to_search == currrent_vertex:
+            print("item found: ",currrent_vertex)
+            return
+        if len(adj_list[currrent_vertex]) > 0:
+            for vertex in adj_list[currrent_vertex]:
+                if visited_nodes[vertex] is False and vertex not in to_visited_stack:
+                    to_visited_stack.append(vertex)
+    print("item not found")
+    return
 
 
 adj_list = [[] for _ in range(4)]
@@ -64,5 +101,8 @@ adj_list = add_vertex(adj_list)
 print_list(adj_list)
 
 print("removing vertex")
-adj_list = remove_vertex(adj_list,1)
+#adj_list = remove_vertex(adj_list,1)
 print_list(adj_list)
+
+search_vertex_bfs(adj_list, 4)
+search_vertex_dfs(adj_list, 0)
--- graphs/adjacentcy_matrix.py
+++ graphs/adjacentcy_matrix.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def add_edge(matrix,i,j):
     matrix[i][j] = 1
     matrix[j][i] = 1
@@ -36,32 +38,54 @@ def add_vertex(matrix):
 
     return updated_matrix
 
+def search_vertex_bfs(matrix, vertex_to_search):
+    queue_to_visit = deque()
+    queue_to_visit.append(0)
+    visited_vertices = [False for _ in range(len(matrix))]
+    print(visited_vertices)
+    while queue_to_visit:
+        current_vertex = queue_to_visit.popleft()
+        print("current vertex:",current_vertex)
+        if current_vertex == vertex_to_search:
+            print("item found: ",vertex_to_search)
+            return
+        visited_vertices[current_vertex] = True
+        current_vertex_neighbours = matrix[current_vertex]
+        #print(current_vertex_neighbours)
+        for i in range(0,len(matrix)):
+            if current_vertex_neighbours[i] == 1 and visited_vertices[i] is False and i not in queue_to_visit:
+                queue_to_visit.append(i)            
+    print("item not found")
+    
 
+#def search_vertex_dfs(matrix, vertex_to_search):
+    
 
-matrix = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+matrix = [[0,1,0,1,0],[1,0,1,0,0],[0,1,0,0,0],[1,0,0,0,1],[0,0,0,1,0]]
 print("Default Graph:")
 print_matrix(matrix)
 #A edges
-add_edge(matrix,0,1)
-add_edge(matrix,0,2)
-add_edge(matrix,0,3)
+#add_edge(matrix,0,1)
+#add_edge(matrix,0,2)
+#add_edge(matrix,0,3)
 
 #B edges
-add_edge(matrix,1,2)
-add_edge(matrix,1,3)
-print("Graph after adding all Edges:")
-print_matrix(matrix)
+#add_edge(matrix,1,2)
+#add_edge(matrix,1,3)
+#print("Graph after adding all Edges:")
+#print_matrix(matrix)
 
 #remove edge edge A to D
-print("removing edge A to D:")
-remove_edge(matrix,0,3)
-print_matrix(matrix)
+#print("removing edge A to D:")
+#remove_edge(matrix,0,3)
+#print_matrix(matrix)
 
-print("Removing vertex")
-matrix=remove_vertex(matrix,3)
-print_matrix(matrix)
+#print("Removing vertex")
+#matrix=remove_vertex(matrix,3)
+#print_matrix(matrix)
 
-print("adding vertex:")
-matrix = add_vertex(matrix)
-print_matrix(matrix)
+#print("adding vertex:")
+#matrix = add_vertex(matrix)
+#print_matrix(matrix)
 
+search_vertex_bfs(matrix,4)
